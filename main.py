6e369deaa2c4211478819823121c353b258d78d3
Muhammet Demiray <md3m1ray@gmail.com>
2024-05-05 00:13:58 +0300

    app revised

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 import tkinter as tk
 
-"""
-1	I
-5	V
-10	X
-50	L
-100	C
-500	D
-1000	M
-5000	V̅ (Üzeri çizgili V)
-"""
 
+def convert_integer(s):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
+             'CD': 400, 'CM': 900}
+    i = 0
+    num = 0
+    while i < len(s):
+        if i + 1 < len(s) and s[i:i + 2] in roman:
+            num += roman[s[i:i + 2]]
+            i += 2
+        else:
+            num += roman[s[i]]
+            i += 1
 
-def convert_integer():
-    pass
+    result_label.config(text=str(num))
 
 
 def button_click():
-    pass
+    convert_integer(roman_entry.get())
 
 
 # UI
 root = tk.Tk()
 root.title("Roman Numarals to Integer")
+root.minsize(width=350, height=250)
+root.config(padx=50, pady=50)
 
 # Roma Rakamı Giriş
 roman_label = tk.Label(root, text="Enter Roman Numaral")
-roman_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+roman_label.pack()
 
 roman_entry = tk.Entry()
-roman_entry.grid(row=1, column=0, columnspan=2, pady=15)
+roman_entry.focus()
+roman_entry.pack()
 
 # Çevirme Butonu
 send_button = tk.Button(root, text="Convert", command=button_click)
-send_button.grid(row=2, column=0, columnspan=2, pady=10)
+send_button.pack()
 
 # Sonuç
 result_label = tk.Label(root, text="")
-result_label.grid(row=3, column=0, columnspan=2, pady=10)
+result_label.pack()
 
 root.mainloop()
